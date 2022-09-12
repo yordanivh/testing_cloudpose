ff847e58152c2912b736d597e2e2fc8da94ee61a
Yordan Halachev <yordan.i.halachev@gmail.com>
2022-09-12 16:27:11 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    awsutils = {
+      source = "cloudposse/awsutils"
+      version = "0.14.0"
+    }
+  }
+
+  cloud {
+    organization = "YordanhTfc4b"
+
+    workspaces {
+      name = "000101010"
+    }
+  }
+}
+
+provider "awsutils" {
+  # Configuration options
+}
+
+resource "null_resource" "name" {
+  
+}
